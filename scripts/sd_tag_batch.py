a177bcb52870ad471b8e00affc8fbc27aec5fb3a
RookHyena <36494751+RookHyena@users.noreply.github.com>
2024-05-07 15:51:56 -0700

    Update sd_tag_batch.py

--- scripts/sd_tag_batch.py
+++ scripts/sd_tag_batch.py
@@ -1,19 +1,19 @@
 import gradio as gr
-import unicodedata
+import re #For remove_attention regular expressions
 from modules import scripts, deepbooru
 from modules.processing import process_images
 import modules.shared as shared
 
 
-def remove_accents(input_str):
-    nfkd_form = unicodedata.normalize('NFKD', input_str)
-    return u"".join(c for c in nfkd_form if not unicodedata.combining(c))
+"""
+
+Thanks to Mathias Russ.
+Thanks to RookHyena.
+
+"""
 
 
 class Script(scripts.Script):
-    # :|
-    original_prompt = "Some"
-    interrogator = "Random text"
 
     def title(self):
         return "Img2img batch interrogator"
@@ -26,36 +26,91 @@ class Script(scripts.Script):
         prompt_weight = gr.Slider(
             0.0, 1.0, value=0.5, step=0.1, label="interrogator weight"
         )
+        use_weight = gr.Checkbox(label="Use Weighted Prompt", value=True)
         use_deepbooru = gr.Checkbox(label="Use deepbooru", value=True)
-        return [in_front, prompt_weight, use_deepbooru]
+        no_duplicates = gr.Checkbox(label="Filter Duplicate Prompt Content from Interrogation", value=False)
+        use_negatives = gr.Checkbox(label="Filter Negative Prompt Content from Interrogation", value=False)
+        return [in_front, prompt_weight, use_deepbooru, use_weight, no_duplicates, use_negatives]
 
-    def run(self, p, in_front, prompt_weight, use_deepbooru):
+    # Required to parse information from a string that is between () or has :##.## suffix
+    def remove_attention(self, words):
+        # Define a regular expression pattern to match attention-related suffixes
+        pattern = r":\d+(\.\d+)?"
+        # Remove attention-related suffixes using regex substitution
+        words = re.sub(pattern, "", words)
+        
+        # Replace escaped left parenthesis with temporary placeholder
+        words = re.sub(r"\\\(", r"TEMP_LEFT_PLACEHOLDER", words)
+        # Replace escaped right parenthesis with temporary placeholder
+        words = re.sub(r"\\\)", r"TEMP_RIGHT_PLACEHOLDER", words)
+        # Define a regular expression pattern to match parentheses and their content
+        pattern = r"(\(|\))"
+        # Remove parentheses using regex substitution
+        words = re.sub(pattern, "", words)
+        # Restore escaped left parenthesis
+        words = re.sub(r"TEMP_LEFT_PLACEHOLDER", r"\\(", words)
+        # Restore escaped right parenthesis
+        words = re.sub(r"TEMP_RIGHT_PLACEHOLDER", r"\\)", words)
+        
+        return words.strip()
 
-        # :|
-        if in_front:
-            _check = f"{Script.original_prompt}, ({Script.interrogator}:{prompt_weight})"
-        else:
-            _check = f"({Script.interrogator}:{prompt_weight}), {Script.original_prompt}"
+    def filter_words(self, prompt, negative):
+        # Corrects a potential error where negative is nonetype
+        if negative is None:
+            negative = ""
+        
+        # Split prompt and negative strings into lists of words
+        prompt_words = [word.strip() for word in prompt.split(",")]
+        negative_words = [self.remove_attention(word.strip()) for word in negative.split(",")]
+        
+        # Filter out words from prompt that are in negative
+        filtered_words = [word for word in prompt_words if self.remove_attention(word) not in negative_words]
+        
+        # Join filtered words back into a string
+        filtered_prompt = ", ".join(filtered_words)
+        
+        return filtered_prompt
 
-        if p.prompt not in [_check, Script.interrogator]:
-            Script.original_prompt = remove_accents(p.prompt)
+    def run(self, p, in_front, prompt_weight, use_deepbooru, use_weight, no_duplicates, use_negatives):
+
+        raw_prompt = p.prompt
+        interrogator = ""
 
         # fix alpha channel
-        p.init_images[0] = p.init_images[0].convert('RGB')
+        p.init_images[0] = p.init_images[0].convert("RGB")
 
         if use_deepbooru:
-            prompt = deepbooru.model.tag(p.init_images[0])
+            interrogator = deepbooru.model.tag(p.init_images[0])
         else:
-            prompt = shared.interrogator.interrogate(p.init_images[0])
-        Script.interrogator = prompt
+            interrogator = shared.interrogator.interrogate(p.init_images[0])
+        
+        # Remove duplicate prompt content from interrogator prompt
+        if no_duplicates:
+            interrogator = self.filter_words(interrogator, raw_prompt)
+        # Remove negative prompt content from interrogator prompt
+        if use_negatives:
+            interrogator = self.filter_words(interrogator, p.negative_prompt)
 
-        p.prompt = ""
-        if Script.original_prompt in ["Some", ""]:
-            p.prompt = Script.interrogator
-        elif in_front:
-            p.prompt = f"{Script.original_prompt}, ({Script.interrogator}:{prompt_weight})"
+        if use_weight:
+            if p.prompt == "":
+                p.prompt = interrogator
+            elif in_front:
+                p.prompt = f"{p.prompt}, ({interrogator}:{prompt_weight})"
+            else:
+                p.prompt = f"({interrogator}:{prompt_weight}), {p.prompt}"
         else:
-            p.prompt = f"({Script.interrogator}:{prompt_weight}), {Script.original_prompt}"
+            if p.prompt == "":
+                p.prompt = interrogator
+            elif in_front:
+                p.prompt = f"{p.prompt}, {interrogator}"
+            else:
+                p.prompt = f"{interrogator}, {p.prompt}"
 
         print(f"Prompt: {p.prompt}")
-        return process_images(p)
+
+        processed = process_images(p)
+
+        # Restore the UI elements we modified
+        p.prompt = raw_prompt
+
+        return processed
